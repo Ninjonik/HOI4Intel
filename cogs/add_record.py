510dfc27e8112a04033d8314c427f1b0b79bf069
NinjonikSVK <petricekzatko@gmail.com>
2023-01-17 19:23:28 +0100

    - basic add record command done

--- cogs/add_record.py
+++ cogs/add_record.py
@@ -5,7 +5,6 @@ from discord.ext import commands
 from discord import app_commands
 
 import config
-import presets
 
 
 class add_record(commands.Cog):
@@ -15,8 +14,43 @@ class add_record(commands.Cog):
 
     @app_commands.command(name="add_record")
     async def add_record(self, interaction: discord.Interaction, player: discord.User,
-                        rating: int):
-        await interaction.response.send_message((player.id, rating))
+                         rating: int):
+        if interaction.user.guild_permissions.administrator:
+            if 0 <= rating <= 100:
+                host = interaction.user
+                current_time = datetime.datetime.now()
+                self.cursor.execute("SELECT rating FROM players WHERE discord_id = %s" % interaction.user.id)
+                host_rating = self.cursor.fetchone()
+                if not host_rating:
+                    self.cursor.execute("INSERT INTO players (discord_id, rating, created_at, "
+                                        "updated_at) "
+                                        "VALUES (%s, %s, '%s', '%s')" % (host.id, 1,
+                                                                         current_time, current_time))
+                    self.connection.commit()
+
+                guild = interaction.guild
+                rating_percentage = rating / 100
+                self.cursor.execute("SELECT rating FROM players WHERE discord_id = %s" % player.id)
+                player_rating = self.cursor.fetchone()
+                if not player_rating:
+                    self.cursor.execute("INSERT INTO players (discord_id, rating, created_at, "
+                                        "updated_at) "
+                                        "VALUES (%s, %s, '%s', '%s')" % (player.id, rating_percentage,
+                                                                         current_time, current_time))
+                    self.connection.commit()
+
+                self.cursor.execute("INSERT INTO player_records (player_id, guild_id, host_id, rating, "
+                                    "created_at, "
+                                    "updated_at) "
+                                    "VALUES (%s, %s, %s, %s, '%s', '%s')" % (player.id, guild.id, host.id,
+                                                                             rating_percentage, current_time,
+                                                                             current_time))
+                self.connection.commit()
+                await interaction.response.send_message(f"Player Record for user {player.name} has been "
+                                                        f"successfully created. Previous rating: -")
+
+            else:
+                interaction.response.send_message("Please only enter values in this interval: <0;100>", ephemeral=True)
 
 
 async def setup(client: commands.Bot) -> None:
--- cogs/setup.py
+++ cogs/setup.py
@@ -17,25 +17,26 @@ class setupCommand(commands.Cog):
     async def setupCommand(self, interaction: discord.Interaction, steam_verification: bool,
                            log_channel: discord.TextChannel,
                            verify_role: discord.Role = None):
-        await interaction.response.send_message((steam_verification, log_channel.id, verify_role.id))
-        guild_id = interaction.guild.id
-        guild_name = interaction.guild.name
-        guild_count = interaction.guild.member_count
-        # Establish database connection
-        self.cursor.execute("SELECT guild_id FROM settings WHERE guild_id='%s'" % guild_id)
-        settings = self.cursor.fetchall()
-        current_time = datetime.datetime.now()
-        if not settings:
-            print(f"{presets.prefix()} Guild was not in database - {guild_name}, adding it")
-            self.cursor.execute("INSERT INTO settings (created_at, updated_at, steam_verification, guild_name, "
-                                "guild_id, log_channel, verify_role) VALUES ('%s', "
-                                "'%s', %s, '%s', %s, %s, %s)" % (current_time,
-                                                                 current_time, steam_verification, guild_name,
-                                                                 guild_id, log_channel.id, verify_role.id))
-            self.cursor.execute(
-                "INSERT INTO statistics (guild_id, created_at, updated_at, count) VALUES (%s, '%s', '%s', %s) " % (
-                    guild_id, current_time, current_time, guild_count))
-            self.connection.commit()
+        if interaction.user.guild_permissions.administrator:
+            await interaction.response.send_message((steam_verification, log_channel.id, verify_role.id))
+            guild_id = interaction.guild.id
+            guild_name = interaction.guild.name
+            guild_count = interaction.guild.member_count
+            # Establish database connection
+            self.cursor.execute("SELECT guild_id FROM settings WHERE guild_id='%s'" % guild_id)
+            settings = self.cursor.fetchall()
+            current_time = datetime.datetime.now()
+            if not settings:
+                print(f"{presets.prefix()} Guild was not in database - {guild_name}, adding it")
+                self.cursor.execute("INSERT INTO settings (created_at, updated_at, steam_verification, guild_name, "
+                                    "guild_id, log_channel, verify_role) VALUES ('%s', "
+                                    "'%s', %s, '%s', %s, %s, %s)" % (current_time,
+                                                                     current_time, steam_verification, guild_name,
+                                                                     guild_id, log_channel.id, verify_role.id))
+                self.cursor.execute(
+                    "INSERT INTO statistics (guild_id, created_at, updated_at, count) VALUES (%s, '%s', '%s', %s) " % (
+                        guild_id, current_time, current_time, guild_count))
+                self.connection.commit()
 
 
 async def setup(client: commands.Bot) -> None:
