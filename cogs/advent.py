0114baab3791438f5bbf629626ae71ccd708eb19
NinjonikSVK <petricekzatko@gmail.com>
2023-12-12 22:05:35 +0100

    - advent day 11 added - fixed wuilting field bug

--- cogs/advent.py
+++ cogs/advent.py
@@ -24,6 +24,8 @@ class Advent(commands.Cog):
             9: {"action": self.gift_action_1, "description": "🛷 Christmas sleigh"},
             10: {"action": self.gift_action_3, "description": "🎄 Christmas coins"},
             11: {"action": self.gift_action_3, "description": "🐉 Ninjonik stolen goods"},
+            12: {"action": self.gift_action_3, "description": "🐉 idk what tu put there"},
+            13: {"action": self.gift_action_3, "description": "🎅 idk what to put there"},
         }
         self.cursor, self.connection = config.setup()
         self.redis = redis_connect()
@@ -40,7 +42,7 @@ class Advent(commands.Cog):
 
     async def gift_action_3(self, user):
         await _add_player_name(user.id, user.name, 0.5)
-        self.cursor.execute("UPDATE players SET currency = currency + 4947 WHERE discord_id=%s", (user.id,))
+        self.cursor.execute("UPDATE players SET currency = currency + 2148 WHERE discord_id=%s", (user.id,))
         self.connection.commit()
 
     def get_claimed_users(self, day):
