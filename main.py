5e89195019d276ee71611303e837d2c10d1ba0bd
Peter <petricekzatko@gmail.com>
2024-08-30 09:34:31 +0200

    - tweaked automod values a bit

--- main.py
+++ main.py
@@ -219,15 +219,15 @@ class Client(commands.Bot):
                 response = presets.perspective.comments().analyze(body=analyze_request).execute()
                 toxicityValue = (response["attributeScores"]["TOXICITY"]["summaryScore"]["value"])
 
-                if toxicityValue >= 0.70:
+                if toxicityValue >= 0.75:
                     await message.delete()
-                    if toxicityValue < 0.75:
+                    if toxicityValue < 0.8:
                         punishment = "Original message has been deleted."
-                    elif 0.75 <= toxicityValue < 0.85:
+                    elif 0.8 <= toxicityValue < 0.9:
                         punishment = "Original message has been deleted. You have been timed-outed for 5 minutes."
                         timeMessage = datetime.datetime.now().astimezone() + datetime.timedelta(minutes=5)
                         await member.timeout(timeMessage, reason=f"Inappropriate message with value {toxicityValue}")
-                    elif 0.85 <= toxicityValue < 0.90:
+                    elif 0.9 <= toxicityValue < 0.95:
                         punishment = "Original message has been deleted. You have been timed-outed for 15 minutes."
                         timeMessage = datetime.datetime.now().astimezone() + datetime.timedelta(minutes=15)
                         await member.timeout(timeMessage, reason=f"Inappropriate message with value {toxicityValue}")
